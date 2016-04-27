66f7876744dfb5e377a05a0fa0b0417439ea58ac
Artem Kovchyk <kovchyk@gmail.com>
2016-04-27 14:54:47 +0300

    Tasks 1 and 2 are done? task 3 is in the progress

---
+++ Artem_Kovchyk/02/index.php
@@ -0,0 +1,14 @@
+<?php
+	//task 1
+	$name = "Artem";
+	$age = 30;
+	echo "My name is $name <br/> My age is $age <br/>";
+
+	//task 2
+	$s = 0;
+	$a = 15;
+
+	$s = round( (pow($a, 2) * sqrt(3) / 4), 2);
+
+	echo $s;
+?>
