b1823e19d66172d0d37f39f6db44223a3e717354
timinaleksej <timinaleksej@yandex.ru>
2016-04-27 14:51:17 +0300

    hw # 2

---
+++ Alexey_Timin/02/index.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$name = "Алексей";
+$age = 35;
+echo "Меня зовут $name","<br />";/*Кроме br ничего не работает*/
+echo "Мне $age лет","<hr />";/*Это тоже для удобства*/
+
+$a = 7;
+$s=1/4*$a*sqrt(3);
+echo "$s","<hr />";
