e61dbb7ef304ba6af400b4014aaa3e77a0ed9f09
dmitry <dimjim2809@gmail.com>
2016-05-04 00:20:24 +0300

    First commit: Added 3 files

---
+++ Dmitriy_Krasnov/02/index.php
@@ -0,0 +1,13 @@
+<?php
+    //task 1
+    $name = "Dmitriy";
+    $age = 31;
+    echo "Меня зовут $name <br/>";
+    echo "Мой возраст $age год <br/>";
+
+
+    //tast 2
+    $a = 10;
+    $S = 1/4*$a*sqrt(3);
+    echo $S;
+?>
