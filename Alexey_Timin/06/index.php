10f2592f5bea3b67c4d9eee84195395f510c4bf9
timinaleksej <timinaleksej@yandex.ru>
2016-05-16 23:41:53 +0300

    hw_5_6

---
+++ Alexey_Timin/06/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+function factorial($n)
+{
+    if ($n < 0) {
+        return 0;
+    }
+    if ($n == 0) {
+        return 1;
+    }
+    for ($i = 1, $fact = 1; $i <= $n; $i++) {
+        $fact = $fact * $i;
+    }
+    return $fact;
+}
+echo factorial(6);
+
+echo '<hr>';
+function calendar()
+{
+    date_default_timezone_set('Europe/Kiev');
+    echo "Сегодня - " . date("d F Y") . "<br>";
+    echo "Текущее время - " . date("H:i:s") . "<br>";
+    $c = date("t") - date("d");
+    for ($a = 1; $a <= $c; $a++) {
+        echo date("d-m-Y-l", mktime(0, 0, 0, date("m"), date("d") + $a, date("Y"))) . '<br>';
+    }
+}
+calendar();
+?>
+</body>
+</html>
