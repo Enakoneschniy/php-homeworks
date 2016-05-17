b141aa5570cc9879d6a996ec8b016d0d6ba62eef
timinaleksej <timinaleksej@yandex.ru>
2016-05-17 18:21:19 +0300

    hw_6_all

--- Alexey_Timin/06/index.php
+++ Alexey_Timin/06/index.php
@@ -47,8 +47,36 @@ sluchMass(2,56,20);
 echo '<pre>';
 print_r($arMass);
 
+echo '<hr>';
+function correct($procent)
+{
+global $arNew;
+    $arKor = array(1, 1, 2.5, 5, 6.5, 10, 3.355, 400, 9.43, 15, 184, 99);
+    $arNew = array();
+    if($procent < 0){
+        echo 'ошибка';
+        exit;
+    }
+    if($procent < 0.8 or $procent > 80){
+        echo 'вне диапазона';
+        exit;
+    }
+    $korr = $procent/100;
+    for ($i = 0; $i <= count($arKor); $i++) {
 
-
+        if ($arKor[$i] == 1) {
+            $arNew[] = $arKor[$i];
+        } elseif ($arKor[$i] == 2.5) {
+            $arNew[] = $arKor[$i] * 1.5;
+        } elseif ($arKor[$i] == 10) {
+            $arNew[] = $arKor[$i] * 3;
+        } else
+            $arNew[] = $arKor[$i] + $arKor[$i] * $korr;
+    }
+}
+correct(81);
+echo '<pre>';
+print_r($arNew);
 ?>
 </body>
 </html>
