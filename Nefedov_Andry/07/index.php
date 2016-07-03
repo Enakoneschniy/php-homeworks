26436b249ff3067ccfe7922f82227623525df555
Nefedov Andry <Nefedov_Andry@bk.ru>
2016-07-03 22:35:38 +0300

    Initial commit

---
+++ Nefedov_Andry/07/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>HW07</title>
+    <style type="text/css">
+
+    </style>
+</head>
+
+<body>
+
+<!--Задача 1
+
+-->
+
+<?php
+
+
+?>
+
+<hr>
+
+<!--Задача 2
+
+-->
+
+<?php
+
+
+?>
+
+<hr>
+
+<!--Задача 3
+
+-->
+
+<?php
+
+
+?>
+
+<hr>
+
+<!--Задача 4
+
+-->
+
+<?php
+
+
+?>
+
+<hr>
+
+</body>
+</html>
